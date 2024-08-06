52e9af2221ec7120f25d1400ef09b9256cc79f0d
awais2o <awaisqamar157@gmail.com>
2024-08-06 22:22:54 +0500

    Fix: product list imporve

--- src/Components/ProductTable.js
+++ src/Components/ProductTable.js
@@ -15,13 +15,14 @@ const ProductTable = () => {
         </tr>
       </thead>
       <tbody>
-        {products?.map((product, index) => (
-          <tr key={product._id}>
-            <td>{index + 1}</td>
-            <td>{product.name}</td>
-            <td>{product.price}</td>
-            <td>{product.quantity}</td>
-            {/* <td> <div style={{ display: 'flex', gap: '10px' }}>
+        {products.length > 0 ? (
+          products?.map((product, index) => (
+            <tr key={product._id}>
+              <td>{index + 1}</td>
+              <td>{product.name}</td>
+              <td>{product.price}</td>
+              <td>{product.quantity}</td>
+              {/* <td> <div style={{ display: 'flex', gap: '10px' }}>
                             <div
                               onClick={() => {
                                 setDisplay(true)
@@ -40,8 +41,15 @@ const ProductTable = () => {
                           
                             
                           </div></td> */}
-          </tr>
-        ))}
+            </tr>
+          ))
+        ) : (
+          <>
+            <tr>
+              <td>No Product</td>
+            </tr>
+          </>
+        )}
       </tbody>
     </Table>
   )
